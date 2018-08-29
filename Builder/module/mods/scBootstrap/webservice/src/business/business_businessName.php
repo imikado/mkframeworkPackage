2084615e7142ad61b7d2188879c9a09103b01bd5
imikado <imikado@gmail.com>
2018-08-29 16:34:08 +0200

    mise a jour du builder en version 2.1.4

--- Builder/module/mods/scBootstrap/webservice/src/business/business_businessName.php
+++ Builder/module/mods/scBootstrap/webservice/src/business/business_businessName.php
@@ -1,5 +1,12 @@
 <?php
 class business_VARbusinessNameENDVAR{
 
+	public function sendResponse($tReturn_){
+		if(count($tReturn_)==1){
+			return current($tReturn_);
+		}
+		return $tReturn_;
+	}
+
 	VARbusinessListMethodENDVAR
 }
